5dbc16bb6908201e111e339e80f664e1858255ea
lightlin <lighterlam@gmail.com>
2023-05-09 23:26:54 +0800

    build: init repo

---
+++ src/LinkedList/LinkedList.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{
+		Val:  val,
+		Next: nil,
+	}
+}
+
+func insertNode(n0 *ListNode, P *ListNode) {
+	n1 := n0.Next
+	P.Next = n1
+	n0.Next = P
+}
+
+func removeNode(n0 *ListNode) {
+	if n0.Next == nil {
+		return
+	}
+	P := n0.Next
+	n1 := P.Next
+	n0.Next = n1
+}
+
+func access(head *ListNode, index int) *ListNode {
+	for i := 0; i < index; i++ {
+		if head == nil {
+			return nil
+		}
+		head = head.Next
+	}
+	return head
+}
+
+func findNode(head *ListNode, target int) int {
+	index := 0
+	for head != nil {
+		if head.Val == target {
+			return index
+		}
+		head = head.Next
+		index++
+	}
+	return -1
+}
+
+func main() {
+	n0 := NewListNode(1)
+	n1 := NewListNode(3)
+	n2 := NewListNode(2)
+	n3 := NewListNode(5)
+	n4 := NewListNode(4)
+	// 构建引用指向
+	n0.Next = n1
+	n1.Next = n2
+	n2.Next = n3
+	n3.Next = n4
+	node := access(n0, 3)
+	fmt.Println(node.Val)
+}
